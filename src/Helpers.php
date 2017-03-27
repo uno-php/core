c86cde8a0e8fcba2dfc613da16e8ef5f362e2eba
Ogunmoye Damilola <dami@ogunmoye.com>
2017-03-27 20:31:32 +0100

    add mix function

--- src/Helpers.php
+++ src/Helpers.php
@@ -105,3 +105,32 @@ if(!function_exists('url')) {
         return asset($string);
     }
 }
+
+if(!function_exists('mix')) {
+    function mix($path, $manifest = false, $shouldHotReload = false, $port = '8080')
+    {
+        if (! $manifest) static $manifest;
+        if (! $shouldHotReload) static $shouldHotReload;
+        if (! $manifest) {
+            $manifestPath = public_path('mix-manifest.json');
+            $shouldHotReload = file_exists(public_path('hot'));
+            if (! file_exists($manifestPath)) {
+                throw new Exception(
+                    'The Uno PHP Mix manifest file does not exist. ' .
+                    'Please run "npm run dev" and try again.'
+                );
+            }
+            $manifest = json_decode(file_get_contents($manifestPath), true);
+        }
+        if (! starts_with($path, '/')) $path = "/{$path}";
+        if (! array_key_exists($path, $manifest)) {
+            throw new Exception(
+                "Unknown Uno PHP Mix file path: {$path}. Please check your requested " .
+                "webpack.mix.js output path, and try again."
+            );
+        }
+        return $shouldHotReload
+            ? "http://localhost:{$port}{$manifest[$path]}"
+            : url($manifest[$path]);
+    }
+}
