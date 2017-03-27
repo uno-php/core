0ef21666e499daae511f87cff2112c2082d1d65d
Ogunmoye Damilola <dami@ogunmoye.com>
2017-03-27 19:10:13 +0100

    update config function in Twig, enable view caching

--- src/Core/TemplateEngine.php
+++ src/Core/TemplateEngine.php
@@ -15,7 +15,7 @@ class TemplateEngine
         $loader = new Twig_Loader_Filesystem(resources_path($viewsPath));
 
         $this->twig = new Twig_Environment($loader, array(
-//            'cache' => storage_path($cachePath),
+            'cache' => storage_path($cachePath),
             'debug' => config('app.debug'),
             'strict_variables' => true
         ));
@@ -27,16 +27,16 @@ class TemplateEngine
 
     private function createFunctions()
     {
-        $urlFunction = new Twig_SimpleFunction('url', function ($name){
+        $urlFunction = new Twig_SimpleFunction('url', function ($name) {
             return url($name);
         });
 
-        $assetFunction = new Twig_SimpleFunction('asset', function ($name){
+        $assetFunction = new Twig_SimpleFunction('asset', function ($name) {
             return asset($name);
         });
 
-        $configFunction = new Twig_SimpleFunction('config', function ($name, $default){
-            return config($name, $default);
+        $configFunction = new Twig_SimpleFunction('config', function ($name, $default = '') {
+            return config($name, $default );
         });
 
         $this->twig->addFunction($assetFunction);
